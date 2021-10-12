1fa3a9371c36ed68d7116b4a7b0be93da34b5d91
Piccolo <kabiryusufbashir@gmail.com>
2021-10-12 09:56:36 +0100

    Working on the homepage

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,6 @@
 <template>
     <div>
-        <img src="../assets/logo.jpg" alt="Foundation Logo">
+        <img class="w-16" src="../assets/logo.jpg" alt="Foundation Logo">
         <h1>Zaufanjinba Foundation</h1>
     </div>
 </template>
-
-<style scoped>
-    img{
-        width:250px;
-    }
-</style>
