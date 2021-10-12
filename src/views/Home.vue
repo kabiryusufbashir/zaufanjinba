541a13da0a1be89ec2cdb922750a32662c5627a4
Piccolo <kabiryusufbashir@gmail.com>
2021-10-12 10:53:30 +0100

    added the aims, youth empowerment section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,62 @@
 <template>
-    <div>
-        <img class="w-16" src="../assets/logo.jpg" alt="Foundation Logo">
-        <h1>Zaufanjinba Foundation</h1>
+    <div id="body">
+        <header>
+            <img class="w-48 mx-auto pt-4" src="../assets/logo.png" alt="Foundation Logo">
+            <h1 class="text-3xl mb-4">Zaufanjinba Foundation</h1>
+        </header>
+        <main class="my-4">
+            <section id="banner" class="w-full">
+                <img class="mx-auto w-5/6" src="../assets/banner.jpg" alt="banner image">
+            </section>
+            <section id="intro" class="my-4 w-2/3 mx-auto">
+                <p>
+                    Zaufanjinba foundation has touched the lives of many people within and outside Borno state through community services and youth empowerment.
+                </p>
+            </section>
+            <section id="aims" class="bg-white">
+                <div class="py-8 w-5/6 mx-auto">
+                    <h1 class="text-4xl py-4">Aims &amp; Objectives </h1>
+                    <p>
+                        To improve the quality of life for those group of people that are in greatest of need or those people that are disadvantaged and vulnerable.
+                        To enhance the health and wellbeing of vulnerable people with local communities through education and treatment
+                        Looking for and creating innovative solutions to support and provide care services to the less fortunate and vulnerable groups
+                    </p>
+                </div>
+            </section>
+            <section id="empowerment" class="py-8 mx-auto">
+                <div class="w-5/6 mx-auto">
+                    <h1 class="text-4xl py-4">Youth Empowerment</h1>
+                    <div class="md:grid grid-cols-2 gap-3">
+                        <div class="py-4">
+                            <img src="../assets/beneficiary_001.jpg" alt="Graduation image 001">
+                        </div>
+                        <div class="my-auto">
+                            <p>
+                                Zaufanjinba foundation has sponsored students to study within and outside the country. Some of the students the foundation has sponsored are now Accountants, Engineers, Medical doctors, and pilots working in numerous places
+                            </p>
+                        </div>
+                    </div>
+                    <div class="md:grid grid-cols-2 gap-3">
+                        <div class="py-4">
+                            <img src="../assets/beneficiary_002.jpg" alt="Graduation image 001">
+                        </div>
+                        <div class="my-auto">
+                            <p>
+                                Zaufanjinba foundation has trained over 1500 youths on different ICT training between 2020 and 2021
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
+        <footer>
+
+        </footer>
     </div>
 </template>
+
+<style>
+    #body{
+        background-color: blanchedalmond;
+    }
+</style>
