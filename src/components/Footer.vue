f7d776e454e305bc8339d6bba2bc90b130332578
Piccolo <kabiryusufbashir@gmail.com>
2021-10-14 20:18:15 +0100

    Add Footer component

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,7 +1,8 @@
 <template>
     <footer class="z-40 relative top-40 py-4 px-4 text-center">
-        +2348147738777, +2349125412336
-        Copyright &copy; 2021 Zaufanjinba Foundation
+        +2348147738777, +2349125412336 <br>
+        Copyright &copy; 2021 Zaufanjinba Foundation <br>
+        Designed by <a href="https://teampiccolo.com">Team Piccolo</a>
     </footer>
 </template>
 
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
     <header class="fixed -top-3 z-50 w-full shadow-sm">
-        <img class="w-48 mx-auto pt-4" src="../assets/logo.png" alt="Foundation Logo">
+        <img class="w-28 mx-auto pt-4" src="../assets/logo.png" alt="Foundation Logo">
         <Heading heading="Zaufanjinba Foundation" />
     </header>
 </template>
