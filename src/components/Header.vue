790debbc814c48a08faf37beff74254c3e0cad3d
Piccolo <kabiryusufbashir@gmail.com>
2021-10-14 17:16:30 +0100

    Adding each section to a compoent

---
+++ src/components/Header.vue
@@ -0,0 +1,16 @@
+<template>
+    <header class="fixed -top-3 z-30 w-full shadow-sm">
+        <img class="w-48 mx-auto pt-4" src="../assets/logo.png" alt="Foundation Logo">
+        <Heading heading="Zaufanjinba Foundation" />
+    </header>
+</template>
+
+<script>
+import Heading from './Heading'
+export default {
+    name: 'Header',
+    components: {
+        Heading
+    }
+}
+</script>
---
+++ src/components/Heading.vue
@@ -0,0 +1,12 @@
+<template>
+        <h1 class="text-3xl mb-4 text-gray-600">{{heading}}</h1>
+</template>
+
+<script>
+export default {
+    name: 'Heading',
+    props:{
+        heading: String
+    }
+}
+</script>
