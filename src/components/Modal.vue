864cbeae535357680492492fc359d05dc10e3d1f
Piccolo <kabiryusufbashir@gmail.com>
2021-10-19 19:29:30 +0100

    Create Donate modal component

---
+++ src/components/Modal.vue
@@ -0,0 +1,58 @@
+<template>
+    <div id="modal">
+        <div class="modal-content">
+            <div class="modal-header text-xl text-center font-medium">
+                <span class="close">&times;</span>
+                Donation <br> 
+                <i class="fas fa-donate text-3xl text-green-600"></i>
+            </div>
+            <div class="modal-body flex flex-col">
+                <input v-model="name" class="border border-gray-600 my-2 p-2 rounded-lg" type="text" name="name" placeholder="Name" id="name">    
+                <input v-model="phone" class="border border-gray-600 my-2 p-2 rounded-lg" type="text" name="phone" placeholder="Phone" id="phone">    
+                <input v-model="amount" class="border border-gray-600 my-2 p-2 rounded-lg" type="text" name="amount" placeholder="Amount" id="phone"> 
+                <button class="bg-green-600 text-white py-2 w-1/2 mx-auto rounded-xl">Donate</button>   
+            </div>
+        </div> 
+    </div>    
+</template>
+
+<script>
+export default {
+    name: 'Modal'
+}
+</script>
+
+
+<style scoped>
+    #modal{
+        background-color: rgba(0, 0, 0, 0.7);
+        position: fixed;
+        z-index: 60;
+        left: 0;
+        top: 0;
+        width: 100%;
+        height: 100%;
+        overflow: auto;
+    }
+
+    .modal-content{
+        background-color: #fefefe;
+        margin: 15% auto;
+        padding: 20px;
+        border: 1px solid #888;
+        width: 80%;
+    }
+
+    .close{
+        color: #aaa;
+        float: right;
+        font-size: 28px;
+        font-weight: bold;
+    }
+
+    .close:hover, .close:focus{
+        color: #000;
+        text-decoration: none;
+        cursor: pointer;
+    }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,21 +3,7 @@
         <Header />
         <Main />
         <Footer />
-        <div id="modal">
-            <div class="modal-content">
-                <div class="modal-header text-xl text-center font-medium">
-                    <span class="close">&times;</span>
-                    Donation <br> 
-                    <i class="fas fa-donate text-3xl text-green-600"></i>
-                </div>
-                <div class="modal-body flex flex-col">
-                    <input class="border border-gray-600 my-2 p-2 rounded-lg" type="text" name="name" placeholder="Name" id="name">    
-                    <input class="border border-gray-600 my-2 p-2 rounded-lg" type="text" name="phone" placeholder="Phone" id="phone">    
-                    <input class="border border-gray-600 my-2 p-2 rounded-lg" type="text" name="amount" placeholder="Amount" id="phone"> 
-                    <button class="bg-green-600 text-white py-2 w-1/2 mx-auto rounded-xl">Donate</button>   
-                </div>
-            </div> 
-        </div>  
+        <Modal />          
     </div>
 </template>
 
@@ -25,47 +11,15 @@
 import Header from '../components/Header'
 import Main from '../components/Main'
 import Footer from '../components/Footer'
+import Modal from '../components/Modal'
 
 export default {
     name: 'Home',
     components:{
         Header,
         Main,
-        Footer
+        Footer,
+        Modal
     }
 }
 </script>
-
-<style scoped>
-    #modal{
-        background-color: rgba(0, 0, 0, 0.7);
-        position: fixed;
-        z-index: 60;
-        left: 0;
-        top: 0;
-        width: 100%;
-        height: 100%;
-        overflow: auto;
-    }
-
-    .modal-content{
-        background-color: #fefefe;
-        margin: 15% auto;
-        padding: 20px;
-        border: 1px solid #888;
-        width: 80%;
-    }
-
-    .close{
-        color: #aaa;
-        float: right;
-        font-size: 28px;
-        font-weight: bold;
-    }
-
-    .close:hover, .close:focus{
-        color: #000;
-        text-decoration: none;
-        cursor: pointer;
-    }
-</style>
